e9137d9a73c5267f2218af00c84756facd7f8bd2
hariom <hariomlovewanshi932@gmail.com>
2025-06-21 13:22:44 +0530

    update home page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -24,13 +24,13 @@ const Menu = () => {
                 {/* Submenu */}
                 <ul className="absolute left-0 top-full mt-1 bg-white text-black shadow-md rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 min-w-[160px] z-50">
                   <li className="px-4 py-2 bg-gradient-to-r from-[#FF8E4D] to-[#FF6360] cursor-pointer text-gray-600">
-                    <Link to="/home"><ion-icon name="chevron-forward-outline"></ion-icon>Home 1</Link>
+                    <Link to="/"><ion-icon name="chevron-forward-outline"></ion-icon>Home 1</Link>
                   </li>
                   <li className="px-4 py-2 hover:bg-gradient-to-r from-[#FF8E4D] to-[#FF6360] cursor-pointer text-gray-600">
-                    <Link to="/home"><ion-icon name="chevron-forward-outline"></ion-icon>Home 2</Link>
+                    <Link to="/"><ion-icon name="chevron-forward-outline"></ion-icon>Home 2</Link>
                   </li>
                   <li className="px-4 py-2 hover:bg-gradient-to-r from-[#FF8E4D] to-[#FF6360] cursor-pointer text-gray-600">
-                    <Link to="/home"><ion-icon name="chevron-forward-outline"></ion-icon>Home 3</Link>
+                    <Link to="/"><ion-icon name="chevron-forward-outline"></ion-icon>Home 3</Link>
                   </li>
                 </ul>
               </li>
